bb2777348f872955994f51a1e2b29fd18e77ccd4
Alexander <sasha.izmyloff@mail.ru>
2024-04-14 18:09:44 +0300

    Level 3 completed

---
+++ Assets/Scripts/Silencer.cs
@@ -0,0 +1,21 @@
+﻿using NPC;
+using UnityEngine;
+
+public class Silencer : MonoBehaviour
+{
+    [SerializeField] private AudioSource source;
+    [SerializeField] private float volume;
+
+    private float _buff;
+    public void SilenceStart(string tag)
+    {
+        if (tag != "silence") return;
+        _buff = source.volume;
+        source.volume = volume;
+    }
+    public void SilenceEnd(string tag)
+    {
+        if (tag != "silence") return;
+        source.volume = _buff;
+    }
+}
